ec2bf5c89b4d559bf1079f19572efaa92f8af5a2
Cueva Tomas Santiago <tomassantiagocueva@gmail.com>
2022-12-28 16:40:39 -0300

    ---

--- Scripts/ComplementarioClase4CuevaTomas.R
+++ Scripts/ComplementarioClase4CuevaTomas.R
@@ -1,10 +1,19 @@
 both_na <- function(vector1,vector2){
   #Lo primero que se me ocurrio hacer
-  if(sum(is.na(vector1)) >= 1){
-    print("El vector tiene un valor NA")
+  if((sum(is.na(vector1)) >= 1) & (sum(is.na(vector2)) >= 1)){
+    print("Los dos vectores tienen almenos un valor NA")
   }
   #Lo segundo que se me ocurrio
+  print("---primer vector---")
   for (i in vector1){
+    if(is.na(i)){
+      print("El valor es NA")
+    }else{
+      print(i) 
+    }
+  }
+  print("---segundo vector---")
+  for (i in vector2){
     if(is.na(i)){
       print("El valor es NA")
     }else{
@@ -19,3 +28,4 @@ numbers_float <- c(1.0,1.5,2.0,2.5,NA,3.0,3.5,4.0,4.5,5.0,5.5,6.0,6.5,7.0,7.5,8.
 
 #Llamo a la funcion con los dos vectores
 both_na(vector1 = numbers_float, vector2 = numbers_int)
+
